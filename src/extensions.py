1e3352ac720680cbb9a8b0f0288a20f0b7ae167f
kouhkan <amirkouhkan1@gmail.com>
2024-02-26 22:42:15 +0330

    feat: add marshmallow

--- src/extensions.py
+++ src/extensions.py
@@ -1,10 +1,10 @@
 """Application extensions."""
-
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app=None)
+ma = Marshmallow(app=None)
 migrate = Migrate(app=None, db=db)
 
-
-__all__ = [db, migrate]
+__all__ = [db, ma, migrate]
